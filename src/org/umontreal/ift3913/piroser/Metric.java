514ed771a0575c40f33a7982b14661640fced8f4
Truong Pham <truong@programminh.com>
2013-04-02 14:50:13 -0400

    Added UML Diagrams

--- src/org/umontreal/ift3913/piroser/Metric.java
+++ src/org/umontreal/ift3913/piroser/Metric.java
@@ -2,7 +2,11 @@ package org.umontreal.ift3913.piroser;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-
+/**
+ * This computes the metrics of a class.
+ * @author Truong Pham
+ *
+ */
 public class Metric {
 	private Classe classe;
 	private Model model;
@@ -442,3 +446,67 @@ public class Metric {
 		return 1 + max;
 	}
 }
+/***********************************************************************************
+ * A flower to thank you for not making me move up to school to give you my report.*
+ *                          Oh! And for the :) as well.                            *
+ ***********************************************************************************
+ 
+                   "M,        .mM"
+                     IMIm    ,mIM"
+                     ,MI:"IM,mIMm
+          "IMmm,    ,IM::::IM::IM,          ,m"
+             "IMMIMMIMm::IM:::::IM""==mm ,mIM"
+    __      ,mIM::::::MIM:::::::IM::::mIMIM"
+ ,mMIMIMIIMIMM::::::::mM::::::::IMIMIMIMMM"
+IMM:::::::::IMM::::::M::::::::IIM:::::::MM,
+ "IMM::::::::::MM:::M:::::::IM:::::::::::IM,
+    "IMm::::::::IMMM:::::::IM:::::::::::::IM,
+      "Mm:::::::::IM::::::MM::::::::::::::::IM,
+       IM:::::::::IM::::::MM::::::::::::::::::IM,
+        MM::::::::IM:::::::IM::::::::::::::::::IM
+        "IM::::::::IM:::::::IM:::::::::::::::::IM;.
+         "IM::::::::MM::::::::IM::::::::::mmmIMMMMMMMm,.
+           IM::::::::IM:::::::IM::::mIMIMM"""". .. "IMMMM
+           "IM::::::::IM::::::mIMIMM"". . . . . .,mM"   "M
+            IMm:::::::IM::::IIMM" . . . . . ..,mMM"
+            "IMMIMIMMIMM::IMM" . . . ._.,mMMMMM"
+             ,IM". . ."IMIM". . . .,mMMMMMMMM"
+           ,IM . . . .,IMM". . . ,mMMMMMMMMM"
+          IM. . . .,mIIMM,. . ..mMMMMMMMMMM"
+         ,M"..,mIMMIMMIMMIMmmmMMMMMMMMMMMM"
+         IM.,IMI"""        ""IIMMMMMMMMMMM
+        ;IMIM"                  ""IMMMMMMM
+        ""                         "IMMMMM
+                                     "IMMM
+                                      "IMM,
+                                       "IMM
+                                        "MM,
+                                         IMM,              ______   __
+                        ______           "IMM__        .mIMMIMMIMMIMMIMM,
+                   .,mIMMIMMIMM, ,mIMM,   IMM"""     ,mIM". . . . "IM,..M,
+                 ,IMMM' . . . "IMM.\ "M,  IMM      ,IM". . . .  / :;IM \ M,
+               .mIM' . . .  / .:"IM.\ MM  "MM,    ,M". . .  / .;mIMIMIM,\ M
+              ,IM'. . .  / . .:;,IMIMIMMM  IMM   ,M". .  / .:mIM"'   "IM,:M
+             ,IM'. . . / . .:;,mIM"  `"IMM IMM   IM. .  / .mM"         "IMI
+            ,IM . .  / . .:;,mIM"      "IMMMMM   MM,.  / ,mM            "M'
+            IM'. .  / . .;,mIM"          "IIMMM ,IMIM,.,IM"
+            IM . . / . .,mIM"              IMMMMMMM' """
+            `IM,.  / ;,mIM"                 IIMMM
+             "IMI, /,mIM"                 __IMMM
+               "IMMMM"                   """IMM
+                 ""                         IMM
+                                            IMM__
+                                            IMM"""
+                                            IMM
+                                            IMM
+                                          __IMM
+                                         """IMM
+                                            IMM
+                                            IMM
+                                            IMM__
+                                            IMM"""
+                                            IMM
+                                   Normand  IMM  Veilleux
+                                   
+                     Ouain... sorry je ne suis pas l'auteur original de ce ASCII.
+**/
